970d645e8baab07f6c50f6c22228706661e31c61
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-27 07:49:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push(snape)
+  create_an_array.push("Hyena")
 end
 
 def add_element_to_start_of_array(array, element)
